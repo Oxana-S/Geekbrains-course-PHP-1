804450ae52804202c91f5abce731460c12247535
Oxana <channel.my.honey@gmail.com>
2019-05-21 15:11:40 +0300

    +
    Пример записи короткой формы массива в php

---
+++ exx/запись массива.php
@@ -0,0 +1,22 @@
+<?php
+// Your code here!
+
+
+$db = [
+    ['id' => '1',
+        'url' => 'img/1.jpg',
+        'views' => '30',
+        'title' => 'Картинка 1',
+        'size' => '0'
+    ],
+    ['id' => '8',
+        'url' => 'img/8.jpg',
+        'views' => '5',
+        'title' => 'Картинка 8',
+        'size' => '0'
+    ]
+];
+
+var_dump($db);
+
+
